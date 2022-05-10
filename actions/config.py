f9ece24b9f4af511f632520b41b1a1938e4e7e7e
Obada Khalili <obada.akhalili@gmail.com>
2022-05-10 22:18:42 +0300

    implement actions for diagnosis and initial stories for training poc

---
+++ actions/config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True, override=True)
+
+env = os.environ.get("ENV", "dev")
+infermedica_app_id = os.environ.get("INFERMEDICA_APP_ID", "")
+infermedica_app_key = os.environ.get("INFERMEDICA_APP_KEY", "")
